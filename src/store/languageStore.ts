4e7f1dc95b9a69890757bd476a80dda7f0b1dba9
Lucas C. Dornelas <lucascdornelas@gmail.com>
2025-01-11 14:50:52 -0300

    feat: implement persistent language storage in languageStore; default locale set to Portuguese

--- src/store/languageStore.ts
+++ src/store/languageStore.ts
@@ -1,13 +1,21 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface LanguageState {
   locale: string;
   setLocale: (locale: string) => void;
 }
 
-const useLanguageStore = create<LanguageState>((set) => ({
-  locale: "en",
-  setLocale: (locale) => set({ locale }),
-}));
+const useLanguageStore = create(
+  persist<LanguageState>(
+    (set) => ({
+      locale: "pt",
+      setLocale: (locale) => set({ locale }),
+    }),
+    {
+      name: "language-storage",
+    }
+  )
+);
 
 export default useLanguageStore;
